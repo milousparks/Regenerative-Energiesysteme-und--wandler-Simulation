fe90251a09f2a96925d772768f3cf777707314b9
Milan larsen <milanlarsen@gmail.com>
2023-01-21 12:41:40 +0100

    neuer versuch

--- main.m
+++ main.m
@@ -15,3 +15,11 @@ param = Parameter();
 mdlName = 'Solarzelle_Simulink.slx';
 open('./02_Simulation/Solarzelle_Simulink.slx');
 sim(mdlName);
+
+%%
+for i=1:52
+    c200(i)=out.i_pv.get.Data(1,1,:)
+end
+%%
+plot(out.v_pv.Data,c200)
+grid on
